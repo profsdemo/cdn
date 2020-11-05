878549b89ae924d09d1923082b6db5c45df77a20
profsdemo <72887745+profsdemo@users.noreply.github.com>
2020-11-05 19:14:32 +0100

    😎
    😎

---
+++ relaod.js
@@ -0,0 +1,4 @@
+/* 
+  Fait "Ctrl+Maj+K", colle le script et appuie sur "Entrer". Pour tester, faite "window.rl.reload();" dans la console. 
+*/
+!function(){const win=window;if(!win){throw"Window"}else{win.rl={reload:function(){win.location.reload();}}}}();
