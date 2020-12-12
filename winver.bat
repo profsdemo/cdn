a81d1b3718750d0364089b8d1dfc50e6af18ac9a
profsdemo <72887745+profsdemo@users.noreply.github.com>
2020-12-12 14:14:50 +0100

    Create winver.bat

---
+++ winver.bat
@@ -0,0 +1,10 @@
+@echo off
+
+:: #==============================#
+:: # Display your Windows version #
+:: #==============================#
+
+echo Appuie sur une touche pour afficher la version de votre Windows...
+pause > nul
+
+start winver.exe
